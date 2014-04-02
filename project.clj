0e865fe592d94ba93d137f92900e14c2668282b2
Kurt Vangraefschepe <kvangrae@wavestrike.com>
2014-04-02 16:13:29 -0400

    upgrade to clojure 1.6.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clojure-accumulo/clojure-accumulo "0.1.0-SNAPSHOT"
   :description "Clojure bindings for Apache Accumulo"
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.apache.hadoop/hadoop-client "1.2.1"
                    :exclusions [ant
                                 hsqldb
