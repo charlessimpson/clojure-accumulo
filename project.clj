3f8dcf3043a62ca4f3fc93792bfaec24d09b442f
Charles Simpson <csimpson@gmail.com>
2014-06-24 19:01:25 -0400

    Bump version to 0.2.0 for development

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-accumulo/clojure-accumulo "0.1.0"
+(defproject clojure-accumulo/clojure-accumulo "0.2.0-SNAPSHOT"
   :description "Clojure bindings for Apache Accumulo"
   :url "https://github.com/charlessimpson/clojure-accumulo"
   :license {:name "The MIT License"
